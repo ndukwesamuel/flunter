91262307fcc4ecdba6bdac1a3aae0619fd1c5d17
ndukwesamuel <ndukwesamuel23@yahoo.com>
2022-07-06 09:54:22 +0100

    new pus

--- index.js
+++ index.js
@@ -15,10 +15,57 @@ app.use(express.json());
 // app.use(cors());
 // app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => {
+app.post("/", (req, res) => {
+  console.log(req.body);
+  const { ID, Amount, Currency, CustomerEmail, SplitInfo } = req.body;
+
   // res.json("")
-  res.send("<h1> Samheart</h1>");
-  //   res.json()
+  //   res.send("<h1> Samheart</h1>");
+  const Userdata = {
+    id: ID,
+    Balance: Amount,
+    SplitBreakdown: [
+      {
+        SplitEntityId: "LNPYACC0019",
+        Amount: 5000,
+      },
+      {
+        SplitEntityId: "LNPYACC0011",
+        Amount: 2000,
+      },
+      {
+        SplitEntityId: "LNPYACC0015",
+        Amount: 2000,
+      },
+    ],
+  };
+  res.status(200).json(Userdata);
+});
+
+app.post("/split-payments/compute", (req, res) => {
+  const { ID, Amount, Currency, CustomerEmail, SplitInfo } = req.body;
+
+  // res.json("")
+  //   res.send("<h1> Samheart</h1>");
+  const Userdata = {
+    id: ID,
+    Balance: Amount,
+    SplitBreakdown: [
+      {
+        SplitEntityId: "LNPYACC0019",
+        Amount: 5000,
+      },
+      {
+        SplitEntityId: "LNPYACC0011",
+        Amount: 2000,
+      },
+      {
+        SplitEntityId: "LNPYACC0015",
+        Amount: 2000,
+      },
+    ],
+  };
+  res.status(200).json(Userdata);
 });
 
 app.listen(port, () => {
